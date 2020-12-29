a3b55567768e27fb112f3059a3b0725a21eb9a06
litsynp <nocte55is@gmail.com>
2020-12-29 15:30:54 +0900

    refactor: trim down views with mixins

--- todos/views.py
+++ todos/views.py
@@ -1,35 +1,14 @@
-from rest_framework import mixins
 from rest_framework import generics
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
-class TodoList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class TodoDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-    def get(self,  request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self,  request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self,  request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
