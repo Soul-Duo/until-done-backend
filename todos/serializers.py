85553b6fb7d89eca05aec2e02fee830362d7ae2a
litsynp <nocte55is@gmail.com>
2020-12-29 16:55:21 +0900

    feat: add users API

--- todos/serializers.py
+++ todos/serializers.py
@@ -3,19 +3,10 @@ from rest_framework import serializers
 from .models import Todo
 
 
-# class TodoListSerializer(serializers.ModelSerializer):
-#     user_id = serializers.RelatedField(source='user')
-
-#     class Meta:
-#         model = TodoList
-#         fields = ['user_id', 'title', 'description']
-
-
 class TodoSerializer(serializers.ModelSerializer):
-    # user_id = serializers.RelatedField(source='user', read_only=True)
-    # todolist_id = serializers.RelatedField(source='todolist')
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description',
+        fields = ['id', 'owner', 'title', 'description',
                   'remind_at', 'created_at', 'completed']
